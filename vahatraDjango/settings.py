67ae2adb10e426137ea79d60248c1ae98035654e
unknown <rafa.rotsy@gmail.com>
2022-08-24 11:26:37 +0300

    add ALLOWED_HOSTS

--- vahatraDjango/settings.py
+++ vahatraDjango/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-)&!0d#b7yu*_wxd$7y@1xffuv5&wi9)+3e=0jft&_4w+6_s94a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://testvahatra.herokuapp.com/','127.0.0.1']
 
 
 # Application definition
