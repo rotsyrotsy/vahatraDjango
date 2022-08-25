3cce9178c33d144e1c6a7315360b1d5dc06e991f
unknown <rafa.rotsy@gmail.com>
2022-08-25 10:43:41 +0300

    change db

--- vahatraDjango/settings.py
+++ vahatraDjango/settings.py
@@ -83,20 +83,20 @@ WSGI_APPLICATION = 'vahatraDjango.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql_psycopg2",
-#         "NAME": "vahatra2",
-#         "USER": "postgres",
-#         "PASSWORD": "",
-#         "HOST": "127.0.0.1",
-#         "PORT": "5432",
-#     }
-# }
-
 DATABASES = {
-    'default': dj_database_url.config()
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "vahatra2",
+        "USER": "postgres",
+        "PASSWORD": "",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
+    }
 }
+
+# DATABASES = {
+#     'default': dj_database_url.config()
+# }
 
 
 
