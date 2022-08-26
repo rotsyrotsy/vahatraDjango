23a12c84824c6ed11452017e871a8c7b30e6c16c
rotsyrotsy <80813050+rotsyrotsy@users.noreply.github.com>
2022-08-26 21:06:52 +0300

    float comma to dot

--- vahatraDjango/settings.py
+++ vahatraDjango/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": "vahatra2",
         "USER": "postgres",
-        "PASSWORD": "",
+        "PASSWORD": "root",
         "HOST": "127.0.0.1",
         "PORT": "5432",
     }
