17ae7c17e22eea71b342da9d57a91c2f3b104f2f
rotsyrotsy <80813050+rotsyrotsy@users.noreply.github.com>
2022-08-28 22:10:24 +0300

    add remote database in settings.py

--- vahatraDjango/settings.py
+++ vahatraDjango/settings.py
@@ -84,17 +84,20 @@ WSGI_APPLICATION = 'vahatraDjango.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "vahatra2",
-        "USER": "postgres",
-        "PASSWORD": "root",
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
-    }
-}
+#DATABASES = {
+#    "default": {
+#        "ENGINE": "django.db.backends.postgresql_psycopg2",
+#        "NAME": "vahatra2",
+#        "USER": "postgres",
+#       "PASSWORD": "root",
+#        "HOST": "127.0.0.1",
+#        "PORT": "5432",
+#    }
+#}
 
+DATABASES = {
+    'default': dj_database_url.config()
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
