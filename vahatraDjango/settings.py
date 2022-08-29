2de6a21ef9f54249f1cf356d912b120f6ba7e49a
rotsyrotsy <80813050+rotsyrotsy@users.noreply.github.com>
2022-08-29 15:09:36 +0300

    test debug false

--- vahatraDjango/settings.py
+++ vahatraDjango/settings.py
@@ -16,6 +16,8 @@ import django_heroku
 import dj_database_url
 from decouple import config 
 
+import logging
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -29,7 +31,7 @@ SECRET_KEY = 'django-insecure-)&!0d#b7yu*_wxd$7y@1xffuv5&wi9)+3e=0jft&_4w+6_s94a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['https://app-vahatra.herokuapp.com/','127.0.0.1']
 
 
 # Application definition
@@ -84,20 +86,20 @@ WSGI_APPLICATION = 'vahatraDjango.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "vahatra",
-        "USER": "postgres",
-       "PASSWORD": "root",
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
-    }
-}
-
 #DATABASES = {
-#    'default': dj_database_url.config()
+ #   "default": {
+  #      "ENGINE": "django.db.backends.postgresql_psycopg2",
+   #     "NAME": "vahatra",
+    #    "USER": "postgres",
+     #  "PASSWORD": "root",
+      #  "HOST": "127.0.0.1",
+       # "PORT": "5432",
+    #}
 #}
+
+DATABASES = {
+    'default': dj_database_url.config()
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -159,7 +161,10 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static'),
 ]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'vahatradjango.storage.WhiteNoiseStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -173,3 +178,21 @@ EMAIL_HOST_PASSWORD = "Poushinie"
 EMAIL_USE_TLS = True
 
 django_heroku.settings(locals())
+
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
