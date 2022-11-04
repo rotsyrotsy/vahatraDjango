54f13223f9c6ebde2dd23e725d8e275c77fe14f6
unknown <rafa.rotsy@gmail.com>
2022-11-04 14:54:12 +0300

    changes

--- vahatraDjango/settings.py
+++ vahatraDjango/settings.py
@@ -214,13 +214,6 @@ django_heroku.settings(locals())
 
 # SESSION_ENGINE = "django.contrib.sessions.backends.cache"
 
-# timeout 1h, active le pool de client, traite les erreurs réseau, éfinit l’option TCP_NODELAY sur l’interface de connexion
-# CACHES = {
-#     'default': {
-#         'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
-#         'LOCATION': '127.0.0.1:11211',
-#     }
-# }
 
 def get_cache():
   import os
